c47d8c9fe2dab7234e03244323b35728053dac72
Yerson Dreyer <dreyerg9@gmail.com>
2026-04-25 17:23:20 -0500

    👑 feat: add admin panel with profile, user and admin CRUD operations

---
+++ src/app/admin/profile/page.js
@@ -0,0 +1,13 @@
+
+import AdminPanel from '../components/perfil';
+import AdminLayout from "../layouts/AdminLayout";
+
+
+export default function AdminPage() {
+
+    return (
+        <AdminLayout>
+            <AdminPanel />
+        </AdminLayout>
+    );
+}
